13daedee120ff8dce96dfbccfba8369f89a8f4a1
srinithin <tsrinithin@gmail.com>
2025-11-19 07:24:45 +0530

    profile component completed

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,112 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+    <div className="bg-white min-h-screen text-black p-6 flex flex-col gap-12 items-center justify-center">
+      <div className="flex flex-col gap-6 items-center text-center relative">
+        <div
+          className="absolute -top-10 left-1/2 -translate-x-1/2 w-92 h-92 md:w-75 md:h-80 
+                        bg-[#af47ff] rounded-full blur-3xl opacity-40 animate-pulse"
+        ></div>
+
         <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
+          alt="profile"
+          src="/profile.png"
+          width={260}
+          height={260}
+          className="object-cover cursor-pointer rounded-full z-10 
+                     hover:ring-4 hover:ring-[#af47ff] hover:scale-105 
+                     transition-all duration-300"
         />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+
+        <h1 className="text-4xl md:text-5xl font-bold z-10 leading-tight">
+          Hi, I'm <span className="text-[#af47ff]">Srinithin Thangadurai</span>
+        </h1>
+
+        <p className="text-lg md:text-2xl text-gray-600 z-10">
+          Full-Stack Web Developer
+        </p>
+
+        <p className="text-base md:text-xl max-w-3xl text-gray-700 leading-relaxed z-10 px-2">
+          3+ years of experience delivering responsive, scalable, and
+          high-performance web applications. Built dynamic UIs using React.js,
+          Next.js, TypeScript, and Tailwind CSS, and developed robust backends
+          with Node.js, Express.js, and MongoDB. Experienced in API
+          integrations, authentication flows, dashboard development, and
+          real-time features. Skilled in optimizing performance, accessibility,
+          and cross-browser compatibility in Agile teams.
+        </p>
+      </div>
+
+      <div className="flex flex-col md:flex-row justify-center gap-4 md:gap-8 z-10 w-1/2 md:w-auto">
+        <button
+          className="hover:scale-105 transition-transform bg-[#af47ff] rounded-2xl px-6 py-3 
+                           text-white cursor-pointer flex items-center justify-center gap-2 w-full md:w-auto"
+        >
+          <span>View My Work</span>
+          <Image
+            src="/icons8-arrow-64.png"
+            alt="arrow"
+            width={28}
+            height={28}
+          />
+        </button>
+
+        <button
+          className="hover:scale-105 transition-transform border border-[#dadfe7] 
+                           rounded-2xl px-6 py-3 hover:bg-[#af47ff] hover:text-white text-[#af47ff] cursor-pointer w-full md:w-auto"
+        >
+          Get In Touch
+        </button>
+
+        <button
+          className="hover:scale-105 transition-transform text-[#af47ff] border border-[#dadfe7]
+                           rounded-2xl px-6 py-3 cursor-pointer flex items-center justify-center gap-2 
+                           w-full md:w-auto hover:bg-[#af47ff] hover:text-white"
+        >
+          <span>Download Resume</span>
+          <Image
+            src="/icons8-download-100.png"
+            alt="download"
+            width={28}
+            height={28}
+          />
+        </button>
+      </div>
+
+      <div className="flex gap-4">
+        <Link href="https://github.com/Srinithin-dev" target="blank">
+          <Image
+            src="/icons8-github.gif"
+            alt="github"
+            width={28}
+            height={28}
+            className="cursor-pointer"
+          />
+        </Link>
+        <Link
+          href="https://www.linkedin.com/in/srinithin-thangadurai-1b237a23b/"
+          target="blank"
+        >
+          <Image
+            src="/icons8-linkedin-30.png"
+            alt="linkedin"
+            width={28}
+            height={28}
+            className="cursor-pointer"
+          />
+        </Link>
+        <Link href="" target="blank">
+          <Image
+            src="/icons8-mail-24.png"
+            alt="mail"
+            width={28}
+            height={28}
+            className="cursor-pointer"
+          />
+        </Link>
+      </div>
     </div>
   );
 }
